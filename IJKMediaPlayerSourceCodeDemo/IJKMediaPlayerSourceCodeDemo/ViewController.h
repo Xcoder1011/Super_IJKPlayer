c6dacd3d50dfc86e755593626c5d8e8824bb5c8c
KUN <wushangkun@youqingxu.com>
2017-09-16 18:09:52 +0800

    initial project

---
+++ IJKMediaPlayerSourceCodeDemo/IJKMediaPlayerSourceCodeDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  IJKMediaPlayerSourceCodeDemo
+//
+//  Created by KUN on 2017/9/16.
+//  Copyright © 2017年 lemon. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ IJKMediaPlayerSourceCodeDemo/IJKMediaPlayerSourceCodeDemo/ViewController.m
@@ -0,0 +1,30 @@
+//
+//  ViewController.m
+//  IJKMediaPlayerSourceCodeDemo
+//
+//  Created by KUN on 2017/9/16.
+//  Copyright © 2017年 lemon. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+    self.view.backgroundColor = [UIColor redColor];
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
